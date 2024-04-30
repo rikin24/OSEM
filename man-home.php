61a616a3b933141ce1b75ba6f945c21545e1cb50
R Bhudia <rb549@student.le.ac.uk>
2024-04-30 12:28:06 +0100

    Add CRUD for manager calendar

--- man-home.php
+++ man-home.php
@@ -1,7 +1,57 @@
 <?php
 include "./man-UI.php";
+
+// Fetch calendar data and convert to JSON before displaying on FullCalendar
+$indCalSql = "SELECT id, event_name, event_start, event_end FROM ind_calendar WHERE empCID='$currentID'";
+$indResult = mysqli_query($link, $indCalSql);
+// Employee's Personal Calendar Data
+$indEvents = [];
+while ($row = $indResult->fetch_assoc()) {
+    $indEvents[] = [
+        'id' => $row['id'],
+        'title' => $row['event_name'],
+        'start' => $row['event_start'],
+        'end' => $row['event_end'],
+        'color' => '#0d6efd'
+    ];
+}
+
+$indEventsJson = json_encode($indEvents);
 ?>
 
+<script>
+    // Render calendar with set styles and imported data
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'dayGridMonth,timeGridWeek,timeGridDay',
+                center: 'title',
+                right: 'prev,next today',
+            },
+            events: <?php echo $indEventsJson; ?>,
+            eventMouseEnter: function(mouseEnterInfo) {
+                mouseEnterInfo.el.style.backgroundColor = '#0d60dc';
+            },
+            eventMouseLeave: function(mouseLeaveInfo) {
+                mouseLeaveInfo.el.style.backgroundColor = '#0d6efd';
+            },
+            eventClick: function(info) {
+                window.location.href = 'man-ind-calendar-update.php?id=' + info.event.id;
+            }
+        });
+        calendar.render();
+    });
+</script>
+
 <body>
-    <h2 class="text-dark text-center p-3">Home</h2>
+<h2 class="text-dark text-center p-3">Home - Personal Calendar</h2>
+<div class="d-flex justify-content-center align-items-center">
+    <a href="man-ind-calendar-add.php" class="btn btn-primary">Add Event</a>
+</div>
+<div class="container justify-content-center align-items-center" style="width: 80%">
+    <div id="calendar" style="max-height: 525px;"></div>
+</div>
+<br>
 </body>
---
+++ man-ind-calendar-update.php
@@ -0,0 +1,80 @@
+<?php
+include "./man-UI.php";
+include "./php/man-ind-cal-upd.php";
+?>
+
+<body>
+<h2 class="text-dark text-center p-3">Update Personal Event</h2>
+<div class="container d-flex justify-content-center align-items-center"
+     style="min-height: 20vh">
+    <form class="p-4 rounded-3 bg-white align-items-center"
+          style="width: 50%;"
+          action="php/man-ind-cal-upd.php"
+          method="post">
+        <?php if (isset($_GET['error'])) { ?>
+            <!--Fetch appropriate error-->
+            <div class="alert alert-danger" role="alert">
+                <?php echo $_GET['error']; ?>
+            </div>
+        <?php } else if (isset($_GET['success'])) { ?>
+            <!--Fetch success message-->
+            <div class="alert alert-success" role="alert">
+                <?php echo $_GET['success']; ?>
+            </div>
+        <?php } ?>
+        <input type="text"
+               name="id"
+               value="<?=$indCalData['id']?>"
+               hidden>
+        <div class="form-group">
+            <div class="mb-3">
+                <label for="name">Event Name</label>
+                <input class="form-control"
+                       type="text"
+                       id="eventName"
+                       name="eventName"
+                       value="<?=$indCalData['event_name']?>">
+            </div>
+        </div>
+        <br>
+        <div class="form-group">
+            <div class="mb-3">
+                <label for="date">Event Start Date</label>
+                <input class="form-control"
+                       type="date"
+                       id="eventStart"
+                       name="eventStart"
+                       value="<?=$indCalData['event_start']?>">
+            </div>
+        </div>
+        <br>
+        <div class="form-group">
+            <div class="mb-3">
+                <label for="date">Event End Date</label>
+                <input class="form-control"
+                       type="date"
+                       id="eventEnd"
+                       name="eventEnd"
+                       value="<?=$indCalData['event_end']?>">
+            </div>
+        </div>
+        <input class="form-control"
+               type="text"
+               id="empCID"
+               name="empCID"
+               value="<?=$currentID?>"
+               hidden>
+        <br>
+        <div class="d-flex justify-content-center align-items-center">
+            <button type="submit"
+                    class="btn btn-primary mx-2"
+                    name="update">Update</button>
+            <a href="php/man-ind-cal-rem.php?id=<?=$indCalData['id']?>"
+               class="btn btn-danger mx-1">Delete</a>
+            <br>
+            <a href="./man-home.php" class="btn btn-secondary mx-2">Back</a>
+        </div>
+    </form>
+</div>
+</body>
+
